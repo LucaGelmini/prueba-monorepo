3b226a616079b0ff7d33b4c5f252a18cdd59f9f2
Luca <lucagelmini@github.com>
2024-05-13 00:32:59 -0300

    first commit

---
+++ apps/api/src/index.ts
@@ -0,0 +1,12 @@
+import cors from 'cors'
+import express from 'express'
+
+const PORT = 8080
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (_req, res) => res.send('Hello world'))
+
+app.listen(PORT, () => console.log(`###\nListening to port ${PORT}\n###`))
